37eddb7c06c58bff096b53292dd63a975e967cb6
Yonatan Sahar <jonathan.sahar@gmail.com>
2024-03-05 11:57:15 +0200

    update

---
+++ code/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import pyarrow
+import pandas as pd
+import sklearn
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+df = pd.read_csv("../data/GSE169159_covid2.csv")
+
+import sys
+sys.executable
